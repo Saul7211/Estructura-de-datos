437286bf6cdcfa95c3460198e4523c267e99c5a8
Saul7211 <116695679+Saul7211@users.noreply.github.com>
2023-05-15 08:35:57 -0500

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,1 +1,55 @@
 # Estructura-de-datos
+#include <iostream>
+using namespace std;
+ 
+// Un nodo de lista enlazada
+class Node
+{
+  public:
+    int key;        // campo de datos
+    Node* next;     // puntero al siguiente nodo
+};
+ 
+// Función de utilidad para devolver un nuevo nodo de lista enlazada del heap
+Node* newNode(int key)
+{
+    // asignar un nuevo nodo en un heap y configurar sus datos
+    Node* node = new Node;
+    node->key = key;
+ 
+    // El puntero `.next` del nuevo nodo no apunta a nada
+    node->next = nullptr;
+ 
+    return node;
+}
+ 
+// Función para la implementación de listas enlazadas que contienen tres nodos
+Node* constructList()
+{
+    // construye tres nodos de lista enlazados
+    Node* first = newNode(1);
+    Node* second = newNode(2);
+    Node* third = newNode(3);
+ 
+    // reorganizar los punteros para construir una lista
+    Node* head = first;
+    first->next = second;
+    second->next = third;
+ 
+    // devuelve un puntero al primer nodo de la lista
+    return head;
+}
+ 
+// Función auxiliar para imprimir una lista enlazada
+void printList(Node* head)
+{
+    Node* ptr = head;
+    while (ptr)
+    {
+        cout << ptr->key << " -> ";
+        ptr = ptr->next;
+    }
+ 
+    cout << "nullptr";
+}
+ 
